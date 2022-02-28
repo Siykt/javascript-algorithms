cad3593f555aff30714c6801e56cfa0586969e7f
siykt <cnsiykt@163.com>
2022-02-28 10:23:44 +0800

    init

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 120,
+  // 对象或数组是否追加空格
+  bracketSpacing: true,
+  // 是否使用单引号
+  singleQuote: true,
+  // 行尾逗号,默认none,可选 none|es5|all
+  // es5 包括es5中的数组、对象
+  // all 包括函数对象等所有可选
+  trailingComma: 'all',
+  // 使用分号
+  semi: false,
+}
