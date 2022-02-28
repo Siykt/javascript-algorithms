75f96cc417ae3d51aaede93138110708896ae9d5
siykt <cnsiykt@163.com>
2022-02-28 15:47:33 +0800

    feat: 添加jest依赖

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,11 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
+  plugins: ['@typescript-eslint'],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'sort-imports': ['error', { ignoreCase: true }],
   },
-};
+}
---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  collectCoverage: true,
+  preset: 'ts-jest',
+  restoreMocks: true,
+  testEnvironment: 'node',
+}
