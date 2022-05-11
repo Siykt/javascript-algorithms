496064719cef24a9b9cd556739f64575f0ae304d
siykt <cnsiykt@163.com>
2022-05-11 15:56:34 +0800

    feat: 添加将一个非空的数组根据有效元素的方式转换为一个树的帮助方法

--- src/h/listToTree.test.ts
+++ src/h/listToTree.test.ts
@@ -1,10 +1,10 @@
-import { listToTreeByLevelOrder } from './listToTree'
+import { listToTreeByEffectiveNode, listToTreeByLevelOrder } from './listToTree'
 import { levelOrder } from '../Tree/examples/LevelOrder'
 import { postorderTraversal } from '../Tree/examples/PostorderTraversal'
 import { preorderTraversal } from '../Tree/examples/PreorderTraversal'
 import { inorderTraversal } from '../Tree/examples/InorderTraversal'
 
-describe('将一个非空的数组转换为一个树', () => {
+describe('将一个非空的数组转换为一个树: listToTreeByLevelOrder', () => {
   test('测试输入: [1, 2, 3, 4, 5]', () => {
     const list = [1, 2, 3, 4, 5]
     const root = listToTreeByLevelOrder(list)
@@ -39,9 +39,6 @@ describe('将一个非空的数组转换为一个树', () => {
 
   test('测试输入: [1, null, 2, null, null, null, 3, null, null, null, null, null, null, null, 4]', () => {
     const list = [1, null, 2, null, null, null, 3, null, null, null, null, null, null, null, 4]
-
-    expect(() => listToTreeByLevelOrder(list)).not.toThrowError('非法输入')
-
     const root = listToTreeByLevelOrder(list)
 
     expect(levelOrder(root)).toStrictEqual([[1], [2], [3], [4]])
@@ -60,6 +57,62 @@ describe('将一个非空的数组转换为一个树', () => {
   test('边界测试, 输入: []', () => {
     const list: number[] = []
     const root = listToTreeByLevelOrder(list)
+
+    expect(root).toBeNull()
+  })
+})
+
+describe('将一个非空的数组转换为一个树: listToTreeByEffectiveNode', () => {
+  test('测试输入: [1, 2, 3, 4, 5]', () => {
+    const list = [1, 2, 3, 4, 5]
+    const root = listToTreeByEffectiveNode(list)
+
+    expect(levelOrder(root)).toStrictEqual([[1], [2, 3], [4, 5]])
+    expect(preorderTraversal(root)).toStrictEqual([1, 2, 4, 5, 3])
+    expect(inorderTraversal(root)).toStrictEqual([4, 2, 5, 1, 3])
+    expect(postorderTraversal(root)).toStrictEqual([4, 5, 2, 3, 1])
+  })
+
+  test('测试输入: [1, 2, 3, null, null, 4, 5]', () => {
+    const list = [1, 2, 3, null, null, 4, 5]
+    const root = listToTreeByEffectiveNode(list)
+
+    expect(levelOrder(root)).toStrictEqual([[1], [2, 3], [4, 5]])
+    expect(preorderTraversal(root)).toStrictEqual([1, 2, 3, 4, 5])
+    expect(inorderTraversal(root)).toStrictEqual([2, 1, 4, 3, 5])
+    expect(postorderTraversal(root)).toStrictEqual([2, 4, 5, 3, 1])
+  })
+
+  test('测试输入: [1, null, 2, null, 3, null, 4, null, 5]', () => {
+    const list = [1, null, 2, null, 3, null, 4, null, 5]
+    const root = listToTreeByEffectiveNode(list)
+
+    expect(levelOrder(root)).toStrictEqual([[1], [2], [3], [4], [5]])
+    expect(preorderTraversal(root)).toStrictEqual([1, 2, 3, 4, 5])
+    expect(inorderTraversal(root)).toStrictEqual([1, 2, 3, 4, 5])
+    expect(postorderTraversal(root)).toStrictEqual([5, 4, 3, 2, 1])
+  })
+
+  test('边界测试, 输入: null', () => {
+    const list = null
+    const root = listToTreeByEffectiveNode(list)
+
+    expect(root).toBeNull()
+  })
+
+  test('边界测试, 输入: [1, null, null, 2, 3]', () => {
+    const list = [1, null, null, 2, 3]
+    const root = listToTreeByEffectiveNode(list)
+
+    expect(levelOrder(root)).toStrictEqual([[1]])
+    expect(preorderTraversal(root)).toStrictEqual([1])
+    expect(inorderTraversal(root)).toStrictEqual([1])
+    expect(postorderTraversal(root)).toStrictEqual([1])
+  })
+
+  test('边界测试, 输入: []', () => {
+    const list: number[] = []
+    const root = listToTreeByEffectiveNode(list)
 
     expect(root).toBeNull()
   })
--- src/h/listToTree.ts
+++ src/h/listToTree.ts
@@ -44,3 +44,39 @@ export function listToTreeByLevelOrder(list: List): TreeNode | null {
   }
   return root
 }
+
+/**
+ * 将一个非空的数组根据有效元素的方式转换为一个树
+ * @param list 要转换的列表
+ */
+export function listToTreeByEffectiveNode(list: List): TreeNode | null {
+  if (list === null || list[0] === null || list[0] === undefined) {
+    return null
+  }
+  // 避免修改原数组
+  list = list.slice()
+  // 创建根节点
+  const root = new TreeNode(list.shift() as number)
+  // 使用一个栈来记录当前节点的子节点
+  const nodeList: (TreeNode | null)[] = [root]
+  while (nodeList.length) {
+    // 取出栈顶元素
+    const curNode = nodeList.shift() as TreeNode
+    // 取出左子树的元素
+    const leftVal = list.shift()
+    if (leftVal !== undefined && leftVal !== null) {
+      const leftNode = new TreeNode(leftVal)
+      curNode.left = leftNode
+      // 将有效的元素放入栈中
+      nodeList.push(leftNode)
+    }
+    // 取出右子树的元素
+    const rightVal = list.shift()
+    if (rightVal !== undefined && rightVal !== null) {
+      const rightNode = new TreeNode(rightVal)
+      curNode.right = rightNode
+      nodeList.push(rightNode)
+    }
+  }
+  return root
+}
