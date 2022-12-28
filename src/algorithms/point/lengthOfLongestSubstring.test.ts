6dd610e9410bad118585ff11fd2e6ab2e874d3e1
siykt <cnsiykt@163.com>
2022-12-28 11:45:25 +0800

    feat: 无重复字符的最长子串

---
+++ src/algorithms/point/lengthOfLongestSubstring.test.ts
@@ -0,0 +1,14 @@
+import { lengthOfLongestSubstring } from './lengthOfLongestSubstring'
+
+describe('无重复字符的最长子串', () => {
+  lengthOfLongestSubstring
+  test('abcabcbb', () => {
+    expect(lengthOfLongestSubstring('abcabcbb')).toBe(3)
+  })
+  test('bbbbb', () => {
+    expect(lengthOfLongestSubstring('bbbbb')).toBe(1)
+  })
+  test('pwwkew', () => {
+    expect(lengthOfLongestSubstring('pwwkew')).toBe(3)
+  })
+})
---
+++ src/algorithms/point/lengthOfLongestSubstring.ts
@@ -0,0 +1,26 @@
+/**
+ * 无重复字符的最长子串
+ * @param s 字符串
+ */
+export function lengthOfLongestSubstring(s: string): number {
+  let maxCount = 0
+  const len = s.length
+  if (len <= 1) return len
+  // 保存已经出现的字符串
+  const strHash = new Set<string>()
+  // 前指针
+  let left = 0
+  // 后指针
+  let right = 0
+  while (right < len) {
+    const endStr = s[right++]
+    // 如果存在重复的字符串，推进前指针至重复字符串之后
+    // 并收缩字符串集合
+    while (strHash.has(endStr)) {
+      strHash.delete(s[left++])
+    }
+    strHash.add(endStr)
+    maxCount = Math.max(maxCount, right - left)
+  }
+  return maxCount
+}
