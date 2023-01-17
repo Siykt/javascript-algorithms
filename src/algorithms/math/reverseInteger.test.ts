3217618164a5868e1cc37add66b333a605320f13
siykt <cnsiykt@163.com>
2023-01-17 16:21:57 +0800

    feat: 整数反转 数学除法与取余

--- src/algorithms/math/reverseInteger.test.ts
+++ src/algorithms/math/reverseInteger.test.ts
@@ -1,4 +1,4 @@
-import { reverseIntegerByStringReverse } from './reverseInteger'
+import { reverseIntegerByMath, reverseIntegerByStringReverse } from './reverseInteger'
 
 describe('整数反转 转字符串暴力反转', () => {
   test('123', () => {
@@ -19,5 +19,27 @@ describe('整数反转 转字符串暴力反转', () => {
 
   test('-123123123123', () => {
     expect(reverseIntegerByStringReverse(-123123123123)).toBe(0)
+  })
+})
+
+describe('整数反转 数学除法', () => {
+  test('123', () => {
+    expect(reverseIntegerByMath(123)).toBe(321)
+  })
+
+  test('-123', () => {
+    expect(reverseIntegerByMath(-123)).toBe(-321)
+  })
+
+  test('120', () => {
+    expect(reverseIntegerByMath(120)).toBe(21)
+  })
+
+  test('123123123123', () => {
+    expect(reverseIntegerByMath(123123123123)).toBe(0)
+  })
+
+  test('-123123123123', () => {
+    expect(reverseIntegerByMath(-123123123123)).toBe(0)
   })
 })
--- src/algorithms/math/reverseInteger.ts
+++ src/algorithms/math/reverseInteger.ts
@@ -18,3 +18,21 @@ export function reverseIntegerByStringReverse(x: number): number {
   if (rx > MAX || rx < MIN) return 0
   return rx
 }
+
+// 数学除法与取余
+export function reverseIntegerByMath(x: number): number {
+  // 是否为负数
+  const isNegative = x < 0
+  if (isNegative) {
+    x = Math.abs(x)
+  }
+  let rx = 0
+  while (x) {
+    // 取10位反转
+    rx = rx * 10 + (x % 10)
+    x = Math.floor(x / 10)
+  }
+  rx = isNegative ? -rx : +rx
+  if (rx > MAX || rx < MIN) return 0
+  return rx
+}
