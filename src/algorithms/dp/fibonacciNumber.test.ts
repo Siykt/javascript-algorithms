ba09a883f645dbc77a386c0278ad50d7f065c755
siykt <cnsiykt@163.com>
2023-01-25 19:04:48 +0800

    feat: 动态规划 + 压缩DPTable 版本

--- src/algorithms/dp/fibonacciNumber.test.ts
+++ src/algorithms/dp/fibonacciNumber.test.ts
@@ -1,4 +1,4 @@
-import { fibByDP, fibByRecursion } from './fibonacciNumber'
+import { fibByDP, fibByPrefDP, fibByRecursion } from './fibonacciNumber'
 
 describe('509. 斐波那契数 递归版', () => {
   test('n = 0', () => {
@@ -41,5 +41,27 @@ describe('509. 斐波那契数 动态规划版', () => {
 
   test('n = 30', () => {
     expect(fibByDP(30)).toBe(832040)
+  }, 300)
+})
+
+describe('509. 斐波那契数 动态规划 + 压缩状态版', () => {
+  test('n = 0', () => {
+    expect(fibByPrefDP(0)).toBe(0)
+  }, 300)
+
+  test('n = 1', () => {
+    expect(fibByPrefDP(1)).toBe(1)
+  }, 300)
+
+  test('n = 2', () => {
+    expect(fibByPrefDP(2)).toBe(1)
+  }, 300)
+
+  test('n = 4', () => {
+    expect(fibByPrefDP(4)).toBe(3)
+  }, 300)
+
+  test('n = 30', () => {
+    expect(fibByPrefDP(30)).toBe(832040)
   }, 300)
 })
--- src/algorithms/dp/fibonacciNumber.ts
+++ src/algorithms/dp/fibonacciNumber.ts
@@ -27,3 +27,27 @@ export function fibByDP(n: number): number {
   }
   return dp[n]
 }
+
+/**
+ * 509. 斐波那契数
+ * @description 动态规划 + 压缩DPTable 版本
+ * @param n n项
+ */
+export function fibByPrefDP(n: number): number {
+  // DP + 压缩DPTable
+
+  if (n === 0) return 0
+  if (n === 1) return 1
+
+  // 压缩 DP Table为两个状态
+  let prev = 1
+  let ans = 1
+  for (let i = 3; i <= n; i++) {
+    // 状态转移方程
+    // dp[i] = dp[i - 1] + dp[i - 2]
+    const temp = ans
+    ans = prev + ans
+    prev = temp
+  }
+  return ans
+}
