a3c77c582766474f5d6bd9f74c9d5013e77ebb90
siykt <cnsiykt@163.com>
2023-01-29 22:40:15 +0800

    fix: 添加可用随机性

--- src/algorithms/others/firstBadVersion.test.ts
+++ src/algorithms/others/firstBadVersion.test.ts
@@ -5,15 +5,15 @@ describe('278. 第一个错误的版本', () => {
 
   test('n = 5, bad = 4', () => {
     expect(firstBadVersion(genIsBadVersion(4))(5)).toBe(4)
-  })
+  }, 300)
 
   test('n = 1, bad = 1', () => {
     expect(firstBadVersion(genIsBadVersion(1))(1)).toBe(1)
-  })
+  }, 300)
 
   test('Random', () => {
     const bad = Math.floor(Math.random() * 2000)
-    const n = Math.floor(Math.random() * 2000)
+    const n = Math.floor(Math.random() * 2000) + 2000
     expect(firstBadVersion(genIsBadVersion(bad))(n)).toBe(bad)
-  })
+  }, 300)
 })
