a4b2060a631a95e8e2f2d95c628a15634bffb6cb
siykt <cnsiykt@163.com>
2023-02-06 17:52:24 +0800

    feat: 191. 位1的个数

---
+++ src/algorithms/bit/hammingWeight.ts
@@ -0,0 +1,14 @@
+/**
+ * 191. 位1的个数
+ * @param n 数
+ */
+export function hammingWeight(n: number): number {
+  // 位运算
+  let ans = 0
+  while (n) {
+    ans++
+    // 与等于自身 -1 即为减去一个1
+    n &= n - 1
+  }
+  return ans
+}
