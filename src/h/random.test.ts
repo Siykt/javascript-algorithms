49c326e6a806982ad22f3e73694148cbc14c9762
siykt <cnsiykt@163.com>
2023-02-20 17:40:12 +0800

    feat: 随机Float

--- src/h/random.test.ts
+++ src/h/random.test.ts
@@ -1,4 +1,4 @@
-import { randomInt } from './random'
+import { randomFloat, randomInt } from './random'
 
 describe('随机Int', () => {
   test('0 ~ 1', () => {
@@ -16,6 +16,26 @@ describe('随机Int', () => {
 
     jest.spyOn(global.Math, 'random').mockReturnValue(0.01)
     expect(randomInt(10, 20)).toBe(10)
+    jest.spyOn(global.Math, 'random').mockRestore()
+  })
+})
+
+describe('随机Float', () => {
+  test('0 ~ 1', () => {
+    jest.spyOn(global.Math, 'random').mockReturnValue(0.9)
+    expect(randomFloat()).toBe(0.9)
+
+    jest.spyOn(global.Math, 'random').mockReturnValue(0.1)
+    expect(randomFloat()).toBe(0.1)
+    jest.spyOn(global.Math, 'random').mockRestore()
+  }, 300)
+
+  test('10 ~ 20', () => {
+    jest.spyOn(global.Math, 'random').mockReturnValue(0.99)
+    expect(randomFloat(10, 20)).toBe(19.9)
+
+    jest.spyOn(global.Math, 'random').mockReturnValue(0.01)
+    expect(randomFloat(10, 20)).toBe(10.1)
     jest.spyOn(global.Math, 'random').mockRestore()
   })
 })
--- src/h/random.ts
+++ src/h/random.ts
@@ -6,3 +6,12 @@
 export function randomInt(min = 0, max = 1) {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
+
+/**
+ * 随机Float
+ * @param min 最小值
+ * @param max 最大值
+ */
+export function randomFloat(min = 0, max = 1) {
+  return Math.random() * (max - min) + min
+}
