35d53dd04d41260f734ac712d125a310797e6186
siykt <cnsiykt@163.com>
2023-02-28 16:56:18 +0800

    feat: 2363. 合并相似的物品双指针版

--- src/algorithms/array/mergeSimilarItems.test.ts
+++ src/algorithms/array/mergeSimilarItems.test.ts
@@ -1,4 +1,4 @@
-import { mergeSimilarItems } from './mergeSimilarItems'
+import { mergeSimilarItems, mergeSimilarItemsByDoublePoint } from './mergeSimilarItems'
 
 describe('2363. 合并相似的物品', () => {
   test('items1 = [[1,1],[4,5],[3,8]], items2 = [[3,1],[1,5]]', () => {
@@ -24,6 +24,49 @@ describe('2363. 合并相似的物品', () => {
   test('items1 = [[1,1],[3,2],[2,3]], items2 = [[2,1],[3,2],[1,3]]', () => {
     expect(
       mergeSimilarItems(
+        [
+          [1, 1],
+          [3, 2],
+          [2, 3],
+        ],
+        [
+          [2, 1],
+          [3, 2],
+          [1, 3],
+        ],
+      ),
+    ).toEqual([
+      [1, 4],
+      [2, 4],
+      [3, 4],
+    ])
+  }, 300)
+})
+
+describe('2363. 合并相似的物品 双指针版本', () => {
+  test('items1 = [[1,1],[4,5],[3,8]], items2 = [[3,1],[1,5]]', () => {
+    expect(
+      mergeSimilarItemsByDoublePoint(
+        [
+          [1, 1],
+          [4, 5],
+          [3, 8],
+        ],
+        [
+          [3, 1],
+          [1, 5],
+        ],
+      ),
+    ).toEqual([
+      [1, 6],
+      [3, 9],
+      [4, 5],
+    ])
+  }, 300)
+
+  test('items1 = [[1,1],[3,2],[2,3]], items2 = [[2,1],[3,2],[1,3]]', () => {
+    expect(
+      mergeSimilarItemsByDoublePoint(
         [
           [1, 1],
           [3, 2],
--- src/algorithms/array/mergeSimilarItems.ts
+++ src/algorithms/array/mergeSimilarItems.ts
@@ -16,3 +16,36 @@ export function mergeSimilarItems(items1: number[][], items2: number[][]): numbe
   }
   return ret
 }
+
+/**
+ * 双指针版本
+ */
+export function mergeSimilarItemsByDoublePoint(items1: number[][], items2: number[][]): number[][] {
+  // 排序 + 双指针
+  items1.sort(([a], [b]) => a - b)
+  items2.sort(([a], [b]) => a - b)
+
+  let p1 = 0
+  let p2 = 0
+  const ret: number[][] = []
+  while (p1 < items1.length && p2 < items2.length) {
+    const [v1, w1] = items1[p1]
+    const [v2, w2] = items2[p2]
+    // 检查排序项的 value 大小决定ret的入栈
+    if (v1 === v2) {
+      // 相等即为叠加
+      ret.push([v1, w1 + w2])
+      p1++
+      p2++
+    } else if (v1 > v2) {
+      // 后续放入更小项目即可
+      ret.push([v2, w2])
+      p2++
+    } else {
+      ret.push([v1, w1])
+      p1++
+    }
+  }
+  // 最后合并结果
+  return ret.concat(items1.slice(p1), items2.slice(p2))
+}
