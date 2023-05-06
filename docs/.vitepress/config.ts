22c31b0ecd26bca82acf50bd2a684d07211f8c4b
siykt <cnsiykt@163.com>
2023-05-06 11:16:37 +0800

    feat: add favicon

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,7 +3,13 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: '数据结构与算法',
-  description: '使用javascript/typescript学习数据结构与算法',
+  titleTemplate: ':title - AntPro',
+  description: '使用javascript/typescript学习数据结构与算法，来自AntPro的学习笔记',
+  lang: 'zh-CN',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico', type: 'image/x-icon' }],
+    ['link', { rel: 'shortcut icon', href: '/favicon.ico', type: 'image/x-icon' }],
+  ],
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
